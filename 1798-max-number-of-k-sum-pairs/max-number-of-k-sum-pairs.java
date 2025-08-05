a41fa316332fbd469761c73c7923936e63e07adf
Adithyakrsna <ksadi2017@gmail.com>
2025-08-05 10:39:47 +0530

    Time: 20 ms (44.97%) | Memory: 58 MB (15.75%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -5,30 +5,24 @@ class Solution {
         int j = n-1;
         int i = 0;
         int count=0;
-        while(i<j){
-            
+        while(i<j){        
             if(arr[i]+arr[j]==k){
                 count++;
                 j--;
                 i++;
-                continue;
-            }
-            if(arr[i]+arr[j]>k){
                 
+            }
+            if(arr[i]+arr[j]>k){        
                 j--;
                 
-                continue;
+                
             }
             if(arr[i]+arr[j]<k){
                 
                 i++;
                 
-                continue;
-            }
-            
-            
-
-        
+                
+            }  
         }
         return count;
     }
