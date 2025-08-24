18d39c40385a25226df179a8a16fe4ac6e6041aa
Adithyakrsna <ksadi2017@gmail.com>
2025-08-24 12:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (13.40%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,44 +1,44 @@
 class Solution {
     public int splitArray(int[] arr, int k) {
+        int n = arr.length;
+        int low =0;
+        int high = 0 ;
 
-        
-        int low = 0;
-            int high = 0;
-             int n = arr.length;
-
-            for(int i = 0 ; i< n ;i++){
-                
-                high += arr[i];
-                if(arr[i] > low){
-                    low = arr[i];
-                }
+        for(int i =0 ; i < n ;i++){
+            if(low<arr[i]){
+                low=arr[i];
             }
+            high += arr[i];
+        }
         while(low<=high){
-                int mid = (low+high)/2;
-                if(task(arr, mid)>k){
-                    low = mid+1;
-                }else{
-                    high = mid-1;
-                }
-            }
-            return low;
+            int mid = ( low+high)/2;
 
+            if(canwe(arr, mid)>k){
+                low= mid+1;
+
+            }else{
+                high=mid-1;
+
+
+            }
 
         }
-    
-
-    static int task (int [] arr ,int mid ){
-        int sum = 0;
-        int target  = 1;
+        return low;
+        
+    }
+    static int canwe(int [] arr , int tar ){
         int n = arr.length;
-        for(int i =  0 ; i < n ;i++){
-            if((sum+arr[i])<=mid){
+        int sum =arr[0];
+        int count =1;
+         for(int i = 1 ; i < n ; i++){
+            if(arr[i]+sum <= tar){
                 sum+=arr[i];
             }else{
                 sum=arr[i];
-                target++;
+                count++;
+
             }
-        }
-        return target;
+         }
+         return count;
     }
 }
