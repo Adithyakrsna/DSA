b218591a4b940fc153edfd2fedfe758ee164e62f
Adithyakrsna <ksadi2017@gmail.com>
2025-08-30 19:19:01 +0530

    Time: 1 ms (99.73%) | Memory: 45.2 MB (59.15%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,34 +1,35 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List <String> arr = new ArrayList<>();
-        for(int i = 1 ; i <=n ;i++){
-            if(i%3==0 && i%5==0){
-                arr.add("FizzBuzz");
-            }else if(i%3==0 ){
-                arr.add("Fizz");
-            }else if( i%5==0){
-                arr.add("Buzz");
-            }else{
-                arr.add(Integer.toString(i));
-            }
-            
-        }
-        return arr;
-        // for(int i=1;i<=n;i++){
+        // ArrayList <String> arr = new ArrayList<>();
+        // for(int i = 1 ; i <=n ;i++){
         //     if(i%3==0 && i%5==0){
-        //         ans.add("FizzBuzz");
+        //         arr.add("FizzBuzz");
+        //     }else if(i%3==0 ){
+        //         arr.add("Fizz");
+        //     }else if( i%5==0){
+        //         arr.add("Buzz");
+        //     }else{
+        //         arr.add(Integer.toString(i));
         //     }
-        //     else if(i%3==0){
-        //         ans.add("Fizz");
-        //     }
-        //     else if(i%5==0){
-        //         ans.add("Buzz");
-        //     }
-        //     else{
-        //         ans.add(Integer.toString(i));
-        //     }
+            
         // }
-        // return ans;
+        // return arr;
+        List<String> ans=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                ans.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                ans.add("Fizz");
+            }
+            else if(i%5==0){
+                ans.add("Buzz");
+            }
+            else{
+                ans.add(Integer.toString(i));
+            }
+        }
+        return ans;
         
     }
 }
