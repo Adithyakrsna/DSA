d290ab9db022a26491fe570ab5f54b5b135107ef
Adithyakrsna <ksadi2017@gmail.com>
2025-08-30 19:18:23 +0530

    Time: 1 ms (99.73%) | Memory: 45.1 MB (59.15%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        ArrayList <String> arr = new ArrayList<>();
+        List <String> arr = new ArrayList<>();
         for(int i = 1 ; i <=n ;i++){
             if(i%3==0 && i%5==0){
                 arr.add("FizzBuzz");
@@ -14,6 +14,21 @@ class Solution {
             
         }
         return arr;
+        // for(int i=1;i<=n;i++){
+        //     if(i%3==0 && i%5==0){
+        //         ans.add("FizzBuzz");
+        //     }
+        //     else if(i%3==0){
+        //         ans.add("Fizz");
+        //     }
+        //     else if(i%5==0){
+        //         ans.add("Buzz");
+        //     }
+        //     else{
+        //         ans.add(Integer.toString(i));
+        //     }
+        // }
+        // return ans;
         
     }
 }
