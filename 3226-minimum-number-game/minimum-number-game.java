f71466dd348e04a58b578ac5e1248a1af5a712dd
Adithyakrsna <ksadi2017@gmail.com>
2025-10-03 17:59:17 +0530

    Time: 2 ms (97.58%) | Memory: 45 MB (39.14%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] numberGame(int[] arr) {
-        int n = arr.length;
+        
         Arrays.sort(arr);
         
        
-        for(int i = 1 ;i<n;i+=2){
-            // arr[i] = arr[i-1] ^ arr[i];
-            // arr[i-1] = arr[i-1] ^ arr[i];
-            // arr[i] = arr[i-1] ^ arr[i];
-            int temp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1]  = temp;
+        for(int i = 1 ;i<arr.length;i+=2){
+            arr[i] = arr[i-1] ^ arr[i];
+            arr[i-1] = arr[i-1] ^ arr[i];
+            arr[i] = arr[i-1] ^ arr[i];
+            // int temp = arr[i];
+            // arr[i] = arr[i-1];
+            // arr[i-1]  = temp;
         }
         return arr;
     }
