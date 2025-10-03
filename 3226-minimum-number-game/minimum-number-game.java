b8e6bed129f94c868cb918469147bf211d056d94
Adithyakrsna <ksadi2017@gmail.com>
2025-10-03 17:59:46 +0530

    Time: 2 ms (97.58%) | Memory: 45.3 MB (5.82%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -5,12 +5,12 @@ class Solution {
         
        
         for(int i = 1 ;i<arr.length;i+=2){
-            arr[i] = arr[i-1] ^ arr[i];
-            arr[i-1] = arr[i-1] ^ arr[i];
-            arr[i] = arr[i-1] ^ arr[i];
-            // int temp = arr[i];
-            // arr[i] = arr[i-1];
-            // arr[i-1]  = temp;
+            // arr[i] = arr[i-1] ^ arr[i];
+            // arr[i-1] = arr[i-1] ^ arr[i];
+            // arr[i] = arr[i-1] ^ arr[i];
+            int temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1]  = temp;
         }
         return arr;
     }
