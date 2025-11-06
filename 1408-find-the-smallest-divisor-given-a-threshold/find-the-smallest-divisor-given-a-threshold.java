d85b9a0eb9ef751bb1a8c2cb5f3bf65c5814f36a
Adithyakrsna <ksadi2017@gmail.com>
2025-11-06 11:43:57 +0530

    Time: 6 ms (98.01%) | Memory: 51.1 MB (5.02%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,77 +1,43 @@
 class Solution {
-    
-    //     int low = 1;
-    //     int n = arr.length;
-    //     if (n > limit) return -1;
-    //     int high = 0;
-        
-
-    //     for(int i = 0 ;i <n;i++){
-    //         high = Math.max(high,arr[i]);
-    //     }
-
-    //     while(low<= high){
-    //         int mid = low+(high-low)/2;
-
-    //         if(isTrue(arr,mid,limit)){
-    //             high = mid-1;
-    //         }else{
-    //             low = mid+1;
-    //         }
-
-
-    //     }
-    //     return low;
-
-    // }
-
-    // public boolean isTrue(int [] arr, int mid,int limit){
-    //     int ans = 0;
-
-    //     for(int i = 0 ;i <arr.length;i++){
-    //         ans+= Math.ceil((double)arr[i]/(double)mid);
-    //     }
-
-    //     if(ans<=limit){
-    //         return true;
-    //     }
-    //     return false;
-        private int sumByD(int[] nums, int limit) {
-        // Size of array
-        int n = nums.length; 
-        
-        // Find the summation of division values
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.ceil((double)nums[i] / (double)limit);
-        }
-        return sum;
-    }
-
-    // Function to find the smallest divisor
-    public int smallestDivisor(int[] nums, int limit) {
-        int n = nums.length;
+    public int smallestDivisor(int[] arr, int limit) {
+        int low = 1;
+        int n = arr.length;
         if (n > limit) return -1;
+        int high = 0;
         
-        //Find the maximum element:
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, nums[i]);
+
+        for(int i = 0 ;i <n;i++){
+            high = Math.max(high,arr[i]);
         }
 
-        int low = 1, high = maxi;
+        while(low<= high){
+            int mid = low+(high-low)/2;
 
-        // Apply binary search
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (sumByD(nums, mid) <= limit) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            if(isTrue(arr,mid,limit)){
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
+
+
         }
         return low;
-    
+
+    }
+
+    public boolean isTrue(int [] arr, int mid,int limit){
+        int ans = 0;
+
+        for(int i = 0 ;i <arr.length;i++){
+            // ans+= Math.ceil((double)arr[i]/(double)mid);
+            ans = ans + (arr[i] + mid - 1) / mid;
+        }
+
+        if(ans<=limit){
+            return true;
+        }
+        return false;
+        
     }
 
     
